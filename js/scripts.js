e54e3eb95a06479f90bab769dcb105a858955899
Ben Dibuz <ben.dibuz@gmail.com>
2021-07-14 07:15:17 -0500

    mileage

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
 let total = 12000;
 [...document.getElementsByClassName('form-check-input has-value')].forEach(function(item) {
   item.addEventListener('click', function(e) {
-      console.log('somethings working')
-    if (e.target.checked) {
-      total += parseInt(e.target.value, 10)
-    } else {
-      total -= parseInt(e.target.value, 10)
+      if (e.target.id == "delivery"){
+        if (e.target.checked) {
+          total += parseInt(document.getElementById("delivery-mileage").value)
+        } else {
+          total -= parseInt(document.getElementById("delivery-mileage").value)
+        }
+      }
+      else{
+        if (e.target.checked) {
+          total += parseInt(e.target.value, 10)
+        } else {
+          total -= parseInt(e.target.value, 10)
+        }
     }
     document.getElementById('total-cost').innerHTML = `$${total.toLocaleString("en-US")}.00`
   })
 
 })
 
+
+document.addEventListener('click', e=> {
+  console.log(e.target);
+
+})
