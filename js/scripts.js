057ff79eeb8a4800947efee91d12bb8c9db11086
Pawtang <ben.dibuz@gmail.com>
2021-07-26 11:39:32 -0500

    fix mileage calc

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 let total = 12000;
 const mileageElem = document.getElementById('delivery-mileage');
+let updateVal = 0;
 
 [...document.getElementsByClassName('form-check-input has-value')].forEach(function(item) {
   item.addEventListener('click', function(e) {
-    const mileage = mileageElem.value;
+    let mileage = mileageElem.value;
       if (e.target.id == "delivery"){
         if (parseInt(mileage, 10) >= 0) {
         if (e.target.checked) {
-          total += parseInt(mileage, 10)
+          updateVal = mileageElem.value;
+          total += parseInt(updateVal, 10)
         } else {
-          total -= parseInt(mileage, 10)
+          total -= parseInt(updateVal, 10)
         }
       }
     }
